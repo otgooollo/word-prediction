b1b770a799963b0997b75763ec48525ff72b4f3b
otgooollo <61413991+otgooollo@users.noreply.github.com>
2020-05-01 22:26:02 +0800

    Add files via upload

---
+++ word prediction result.py
@@ -0,0 +1,8 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\ollo\AppData\Local\Programs\Python\Python36\word prediction.py 
+ugee oruulna uu :Хорт
+Хорт муу санаат хэдгэнэ хоёрыг дуудуудлж гэнэ. 
+
+>>> 
